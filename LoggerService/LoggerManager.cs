fe8dbbf73823be84acba8c948b74e0768a05b00c
Rati.Shoshikelashvili6911@ens.tsu.edu.ge <Rati.Shoshikelashvili6911@ens.tsu.edu.ge>
2021-07-31 17:19:28 +0400

    Finished configuring the logging library

--- LoggerService/LoggerManager.cs
+++ LoggerService/LoggerManager.cs
@@ -1,8 +1,30 @@
-﻿using System;
+﻿using Contracts;
+using NLog;
+using System;
 
-namespace LoggerService
+namespace LoggerService 
 {
-    public class LoggerManager
+    public class LoggerManager : ILoggerManager
     {
+        private static ILogger logger = LogManager.GetCurrentClassLogger();
+        public void LogDebug(string message)
+        {
+            logger.Debug(message);
+        }
+
+        public void LogError(string message)
+        {
+            logger.Error(message);
+        }
+
+        public void LogInfo(string message)
+        {
+            logger.Info(message);
+        }
+
+        public void LogWarn(string message)
+        {
+            logger.Warn(message);
+        }
     }
 }
--- PhysicalPersons/Extensions/ServiceExtensions.cs
+++ PhysicalPersons/Extensions/ServiceExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Contracts;
+using LoggerService;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -26,5 +28,9 @@ namespace PhysicalPersons.Extensions
          {
 
          });
+
+        //Logger configuration
+        public static void ConfigureLoggerService(this IServiceCollection services)
+            => services.AddScoped<ILoggerManager, LoggerManager>();
     }
 }

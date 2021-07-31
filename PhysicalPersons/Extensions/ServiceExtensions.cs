c6182c7e3a2a58cf30b913639c7757bc379d7fef
Rati.Shoshikelashvili6911@ens.tsu.edu.ge <Rati.Shoshikelashvili6911@ens.tsu.edu.ge>
2021-07-31 16:49:24 +0400

    Added extensions for services and registered/configured them

---
+++ PhysicalPersons/Extensions/ServiceExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhysicalPersons.Extensions
+{
+    //Extensions class resposible for configuring service setup methods.
+    public static class ServiceExtensions
+    {
+        //TODO: Once published, CORS setup should be modified to allow requests only from specific origins. 
+        public static void ConfigureCors(this IServiceCollection services) =>
+         services.AddCors(options =>
+         {
+             options.AddPolicy("CorsPolicy", builder =>
+             builder.AllowAnyOrigin()
+             .AllowAnyMethod()
+             .AllowAnyHeader());
+         });
+
+        //This will be useful during the deployment to IIS servers
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+         services.Configure<IISOptions>(options =>
+         {
+
+         });
+    }
+}

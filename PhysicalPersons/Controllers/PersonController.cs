cfb21073e406474832721f7c4c284d75feca3491
Rati.Shoshikelashvili6911@ens.tsu.edu.ge <Rati.Shoshikelashvili6911@ens.tsu.edu.ge>
2021-08-04 08:11:03 +0400

    Configured exception handler

--- PhysicalPersons/Controllers/PersonController.cs
+++ PhysicalPersons/Controllers/PersonController.cs
@@ -29,35 +29,27 @@ namespace PhysicalPersons.Controllers
         [HttpGet("{id}")]
         public IActionResult GetPerson(int id)
         {
-            try
-            {
-                var person = _unitOfWork.Person.GetPerson(id, false);
-                person.City = _unitOfWork.City.GetCityByPerson(person, false);
-                person.PhoneNumbers = _unitOfWork.PhoneNumber.GetPhoneNumbersByPerson(person, false).ToList();
+            var person = _unitOfWork.Person.GetPerson(id, false);
+            person.City = _unitOfWork.City.GetCityByPerson(person, false);
+            person.PhoneNumbers = _unitOfWork.PhoneNumber.GetPhoneNumbersByPerson(person, false).ToList();
                 
-                var personRelationsFromIds = _unitOfWork.PersonRelation.GetPersonRelationsFrom(person, false).Select(p=> p.RelatedToId).ToList();
-                var relatedFromPersons = _unitOfWork.Person.GetPersonsByIds(personRelationsFromIds, false).ToList();
-                var relatedFromDto = relatedFromPersons.AsEnumerable().Select(p => _mapper.Map<RelatedPersonDto>(p, opt =>
-    opt.AfterMap((src, dest) => dest.RelationType = _unitOfWork.PersonRelation.GetRelationType(person.Id, p.Id, false).RelationType)));
+            var personRelationsFromIds = _unitOfWork.PersonRelation.GetPersonRelationsFrom(person, false).Select(p=> p.RelatedToId).ToList();
+            var relatedFromPersons = _unitOfWork.Person.GetPersonsByIds(personRelationsFromIds, false).ToList();
+            var relatedFromDto = relatedFromPersons.AsEnumerable().Select(p => _mapper.Map<RelatedPersonDto>(p, opt =>
+opt.AfterMap((src, dest) => dest.RelationType = _unitOfWork.PersonRelation.GetRelationType(person.Id, p.Id, false).RelationType)));
 
-                var personRelationsToIds = _unitOfWork.PersonRelation.GetPersonRelationsTo(person, false).Select(p => p.RelatedFromId).ToList();
-                var relatedToPersons = _unitOfWork.Person.GetPersonsByIds(personRelationsToIds, false);
-                var relatedToDto = relatedToPersons.AsEnumerable().Select(p => _mapper.Map<RelatedPersonDto>(p, opt =>
-    opt.AfterMap((src, dest) => dest.RelationType = _unitOfWork.PersonRelation.GetRelationType(p.Id, person.Id, false).RelationType)));
+            var personRelationsToIds = _unitOfWork.PersonRelation.GetPersonRelationsTo(person, false).Select(p => p.RelatedFromId).ToList();
+            var relatedToPersons = _unitOfWork.Person.GetPersonsByIds(personRelationsToIds, false);
+            var relatedToDto = relatedToPersons.AsEnumerable().Select(p => _mapper.Map<RelatedPersonDto>(p, opt =>
+opt.AfterMap((src, dest) => dest.RelationType = _unitOfWork.PersonRelation.GetRelationType(p.Id, person.Id, false).RelationType)));
 
 
-                var personDto = _mapper.Map<PersonDto>(person, opt => opt.AfterMap((src, dest) => 
-                { 
-                    dest.RelatedFrom = relatedFromDto;
-                    dest.RelatedTo = relatedToDto;
-                }));
-                return Ok(personDto);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"An error occured in the {nameof(GetPerson)} action {ex}");
-                return StatusCode(500, "Internal server error");
-            }
+            var personDto = _mapper.Map<PersonDto>(person, opt => opt.AfterMap((src, dest) => 
+            { 
+                dest.RelatedFrom = relatedFromDto;
+                dest.RelatedTo = relatedToDto;
+            }));
+            return Ok(personDto);
         }
     }
 }
--- PhysicalPersons/Startup.cs
+++ PhysicalPersons/Startup.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using CompanyEmployees.Extensions;
+using Contracts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
@@ -41,13 +43,14 @@ namespace PhysicalPersons
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerManager logger)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.ConfigureExceptionHandler(logger);
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
